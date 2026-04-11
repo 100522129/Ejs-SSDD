0a106d65c4f6fde16560e4287b3cbdfab9bf3549
2pabl0 <pabloingeniero31@proton.me>
2026-04-11 16:38:43 +0200

    proxy listo

--- ej2/src/proxy-sock.c
+++ ej2/src/proxy-sock.c
@@ -11,12 +11,9 @@
 #include "mensajes.h"
 #include "cJSON.h"
 
-/* Notas Importantes:
-    - El servidor escucha siempre por el mismo puerto
-    - Ya no se envian structs, ahora se envian jsons
-*/
+#define MAX_RESPONSE_SIZE 4096
 
-char* crear_json (struct peticion req){ // Falta tratar errores y refactorizar
+char* crear_json (struct peticion req){ 
     // Comprobacion basica
     if (req.op < 0 || req.op > 5){
         return NULL;
@@ -25,12 +22,12 @@ char* crear_json (struct peticion req){ // Falta tratar errores y refactorizar
     // 1. Creo el JSON
     cJSON *obj = cJSON_CreateObject();
 
-    if (req.op == 0){ // EL tipo de operacion es destroy
+    if (req.op == OP_INIT){ // EL tipo de operacion es destroy
         // Relleno el JSON
         cJSON_AddNumberToObject(obj, "op", OP_INIT);
     }
 
-    else if (req.op == 1){ // La operacion es set_value
+    else if (req.op == OP_SET){ // La operacion es set_value
         // Relleno el JSON
         cJSON_AddNumberToObject(obj, "op", OP_SET);
         cJSON_AddStringToObject(obj, "key", req.key);
@@ -52,12 +49,12 @@ char* crear_json (struct peticion req){ // Falta tratar errores y refactorizar
         cJSON_AddItemToObject(obj, "value3", paquete);
     }
    
-    else if (req.op == 2){ // La operacion es get_value
+    else if (req.op == OP_GET){ // La operacion es get_value
         cJSON_AddNumberToObject(obj, "op", OP_GET);
         cJSON_AddStringToObject(obj, "key", req.key);
     }
 
-    else if (req.op == 3){ // La operacion es modify_value
+    else if (req.op == OP_MODIFY){ // La operacion es modify_value
         // Relleno el JSON
         cJSON_AddNumberToObject(obj, "op", OP_MODIFY);
         cJSON_AddStringToObject(obj, "key", req.key);
@@ -79,12 +76,12 @@ char* crear_json (struct peticion req){ // Falta tratar errores y refactorizar
         cJSON_AddItemToObject(obj, "value3", paquete);
     }
 
-    else if (req.op == 4){ // La operacion es delete_key
+    else if (req.op == OP_DELETE){ // La operacion es delete_key
         cJSON_AddNumberToObject(obj, "op", OP_DELETE);
         cJSON_AddStringToObject(obj, "key", req.key);
     }
 
-    else if (req.op == 5){ // La operacon es exist
+    else if (req.op == OP_EXIST){ // La operacon es exist
         cJSON_AddNumberToObject(obj, "op", OP_EXIST);
         cJSON_AddStringToObject(obj, "key", req.key);
     }
@@ -99,12 +96,25 @@ char* crear_json (struct peticion req){ // Falta tratar errores y refactorizar
 }
 
 struct respuesta traducir_json_response (char* json_response){ // Falta tratar errores
+
+    struct respuesta respuesta_servidor;
+    respuesta_servidor.resultado = -1; // Valor por defecto por en caso de error
+
     // 1. Parseo el JSON con cJSON
     cJSON *obj = cJSON_Parse(json_response);
+    if (obj == NULL) {
+            perror("Error al parsear el JSON de la respuesta");
+            return respuesta_servidor;
+        }
 
     // 2. Relleno los campos de la respuesta
-    struct respuesta respuesta_servidor;
-    respuesta_servidor.resultado = cJSON_GetObjectItem(obj, "result")->valueint;
+    cJSON *result = cJSON_GetObjectItem(obj, "result");
+    if (result == NULL) {
+        perror("Error: campo 'result' no encontrado en la respuesta");
+        cJSON_Delete(obj);
+        return respuesta_servidor;
+    }
+    respuesta_servidor.resultado = result->valueint;
 
     // 2.1 Miro si la operacion es get_value (es la unica que tiene mas campos)
     if (cJSON_GetArraySize(obj)> 1){
@@ -143,38 +153,34 @@ int send_recv(char* request, char* response) {
         return -2;
     }
 
-    // 2. Resolver el host con getaddrinfo
-    struct addrinfo *res; // es la salida, getaddrinfo escribe ahi
-    struct addrinfo hints; // es la entrada, para decir el tipo de conexion que queremos (IPv4, TCP)
-
-    // 2.1 Quito la basura que pueda haber al declarar la variable hints
-    memset(&hints, 0, sizeof(hints));
-    
-    // 2.2 Indico la informacion de la conexion
-    hints.ai_family = AF_INET; // Significa IPv4
-    hints.ai_socktype = SOCK_STREAM; // Significa TCP
-
-    // 2.3 LLamo a getaddrinfo (getaddrinfo necesita un puntero al puntero res)
-    if (getaddrinfo(ip_servidor, puerto_servidor, &hints, &res) !=0){
-        perror("Error al usar getaddrinfo");
+    // 2. Resolver el host con gethostbyname
+    struct hostent *hp; 
+    struct sockaddr_in server_addr;
+    hp = gethostbyname(ip_servidor);
+    if (hp == NULL) {
+        perror("Error en gethostbyname");
         return -2;
     }
 
-    // 3. Crear socket y hacer connect (ya tengo toda la info en res)
+    // 2.1 Relleno la estructura de direccion
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET; // IPV4
+    server_addr.sin_port = htons(atoi(puerto_servidor)); // Puerto
+    memcpy(&server_addr.sin_addr, hp->h_addr_list[0], hp->h_length);
+
+    // 3. Crear socket y hacer connect
 
     // 3.1 Para crear el socket necesito el tipo de direccion, tipo de socket y protocolo
-    int sock = socket(res->ai_family, res->ai_socktype, 0);
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1){
         perror("Error al crear el socket");
-        freeaddrinfo(res);
         return -2;
     }
 
     // 3.2 Me conecto al servidor (necesito su IP y PUERTO, viene ya todo en ai_addr)
-    if (connect(sock, res->ai_addr, res->ai_addrlen) == -1) {
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
         perror("Error al conectar");
         close(sock);
-        freeaddrinfo(res);
         return -2;
     }
 
@@ -187,39 +193,66 @@ int send_recv(char* request, char* response) {
     uint32_t longitud_request_final = htonl(longitud_request);
     
     // 4.3 Envio la longitud de la request
-    send(sock, &longitud_request_final, sizeof(longitud_request_final), 0);
+    if (send(sock, &longitud_request_final, sizeof(longitud_request_final), 0) == -1) {
+        perror("Error al enviar la longitud de la request");
+        close(sock);
+        return -2;
+    }
 
     // 4.4 Envio la request
-    send(sock, request, longitud_request, 0);
+    if (send(sock, request, longitud_request, 0) == -1) {
+        perror("Error al enviar la request");
+        close(sock);
+        return -2;
+    }
 
     // 5. Recibir los 4 Bytes de longitud + el JSON response
 
     // 5.1 Primero recibo la longitud de la response
     uint32_t longitud_response;
-    recv(sock, &longitud_response, sizeof(longitud_response), 0);
+    if (recv(sock, &longitud_response, sizeof(longitud_response), 0) == -1) {
+        perror("Error al recibir la longitud de la response");
+        close(sock);
+        return -2;
+    }
 
     // 5.2 Convierto la longitud a formato local
     int longitud_response_final = ntohl(longitud_response);
 
-    // 5.3 Recibo el JSON
-    recv(sock, response, longitud_response_final, 0);
+    // 5.3 Compruebo que la respones cabe en el buffer
+    if (longitud_response_final >= MAX_RESPONSE_SIZE) {
+        perror("Error: la response es demasiado grande para el buffer");
+        close(sock);
+        return -2;
+    }
+
+    // 5.4 Recibo el JSON
+    if (recv(sock, response, longitud_response_final, 0) == -1) {
+        perror("Error al recibir la response");
+        close(sock);
+        return -2;
+    }
+    response[longitud_response_final] = '\0'; // Para que al parsear cJSON no falle
 
     // 6. Cerrar el socket
     close(sock);
-    freeaddrinfo(res);
     return 0;
 }
 
 int destroy(void) {
 
     struct peticion req;
-    char res [4096];
+    char res [MAX_RESPONSE_SIZE];
 
     // 1. Empaqueto la petición
     req.op = OP_INIT;
 
     // 2. Creo el JSON para la request
     char* json_request = crear_json(req);
+    if (json_request == NULL) {
+        perror("Error al crear el JSON");
+        return -2;
+    }
 
     // 3. Envio la request en formato JSON y recibo la response en formato JSON
     if (send_recv(json_request, res) == -2){
@@ -238,7 +271,7 @@ int destroy(void) {
 int set_value(char *key, char *value1, int N_value2, float *V_value2, struct Paquete value3) {
 
     struct peticion req;
-    char res [4096];
+    char res [MAX_RESPONSE_SIZE];
 
     // 1. Empaquetar la petición
     req.op = OP_SET;
@@ -255,6 +288,10 @@ int set_value(char *key, char *value1, int N_value2, float *V_value2, struct Paq
 
     // 3. Paso la request a JSON
     char* json_request = crear_json(req);
+    if (json_request == NULL) {
+        perror("Error al crear el JSON");
+        return -2;
+    }
 
     // 4. Envio la request en formato JSON y recibo la response en formato JSON
     if (send_recv(json_request, res) == -2){
@@ -276,7 +313,7 @@ int get_value(char *key, char *value1, int *N_value2, float *V_value2, struct Pa
         return -1;
     }
     struct peticion req;
-    char res [4096];
+    char res [MAX_RESPONSE_SIZE];
 
     // 1. Empaquetar la petición
     req.op = OP_GET;
@@ -284,7 +321,11 @@ int get_value(char *key, char *value1, int *N_value2, float *V_value2, struct Pa
     
     // 2. Paso la request a JSON
     char* json_request = crear_json(req);
-    
+    if (json_request == NULL) {
+        perror("Error al crear el JSON");
+        return -2;
+    }
+
     // 3. Envio la request en formato JSON y recibo la response en formato JSON
     if (send_recv(json_request, res) == -2){
         free(json_request);
@@ -311,7 +352,7 @@ int get_value(char *key, char *value1, int *N_value2, float *V_value2, struct Pa
 int modify_value(char *key, char *value1, int N_value2, float *V_value2, struct Paquete value3) {
 
     struct peticion req;
-    char res [4096];
+    char res [MAX_RESPONSE_SIZE];
 
     // 1. Empaquetar la petición
     req.op = OP_MODIFY;
@@ -328,6 +369,10 @@ int modify_value(char *key, char *value1, int N_value2, float *V_value2, struct 
     
     // 2. Paso la request a JSON 
     char* json_request = crear_json(req);
+    if (json_request == NULL) {
+        perror("Error al crear el JSON");
+        return -2;
+    }
 
     // 3. Envio la request en formato JSON y recibo la response en formato JSON
     if (send_recv(json_request, res) == -2){
@@ -349,7 +394,7 @@ int delete_key(char *key) {
         return -1;
     }
     struct peticion req;
-    char res[4096];
+    char res[MAX_RESPONSE_SIZE];
 
     // 1. Empaquetar la petición
     req.op = OP_DELETE;
@@ -357,6 +402,10 @@ int delete_key(char *key) {
 
     // 2. Paso la request a JSON
     char* json_request = crear_json(req);
+    if (json_request == NULL) {
+        perror("Error al crear el JSON");
+        return -2;
+    }
 
     // 3. Envio la request en formato JSON y recibo la response en formato JSON
     if (send_recv(json_request, res) == -2){
@@ -379,7 +428,7 @@ int exist(char *key) {
     }
     
     struct peticion req;
-    char res [4096];
+    char res [MAX_RESPONSE_SIZE];
 
     // 1. Empaquetar la petición
     req.op = OP_EXIST;
@@ -387,6 +436,10 @@ int exist(char *key) {
 
     // 2. Paso la request a JSON
     char* json_request = crear_json(req);
+    if (json_request == NULL) {
+        perror("Error al crear el JSON");
+        return -2;
+    }
 
     // 3. Envio la request en formato JSON y recibo la response en formato JSON
     if (send_recv(json_request, res) == -2){
